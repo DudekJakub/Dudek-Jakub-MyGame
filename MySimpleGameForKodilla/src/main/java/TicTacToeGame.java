256d4ab406d2398d42c25c76a0203d011e3dc854
Jakub Dudek <jakub.dudek94@gmail.com>
2021-10-19 10:19:57 +0200

    Commit nr 1 - nowa gra Kółko i Krzyżyk - stan na 19.10.2021

---
+++ MySimpleGameForKodilla/src/main/java/TicTacToeGame.java
@@ -0,0 +1,15 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class TicTacToeGame extends Application {
+
+    @Override
+    public void start(Stage mainStage) throws Exception {
+
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
